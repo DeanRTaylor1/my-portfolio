623d5042e21e8c8b3de897208b8bd452b70a37fd
deanrtaylor1 <deanrtaylor@hotmail.com>
2022-12-23 20:07:37 +0700

    increased font size of H1 on main div

--- @modules/common/Components/Home/Main.tsx
+++ @modules/common/Components/Home/Main.tsx
@@ -18,7 +18,7 @@ const Main: React.FC = () => {
     >
       <div className='w-full h-full bg-zinc-300 flex items-center justify-center gap-4 md:gap-24 p-8 md:w-2/4'>
         <div className='flex flex-col w-full items-start justify-center font-extrabold drop-shadow-2xl text-black text-2xl gap-4 underline decoration-emerald-400 decoration-4 underline-offset-4'>
-          <h2 className='hover:cursor-pointer hover:opacity-75 lg:text-4xl '>
+          <h2 className='hover:cursor-pointer hover:opacity-75 text-6xl lg:text-6xl '>
             Dean Taylor
           </h2>
           <h2 className='text-xl hover:cursor-pointer hover:opacity-75 lg:text-3xl'>
